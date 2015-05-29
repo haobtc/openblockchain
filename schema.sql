f5b53d7fbeac978b6c1dff6820351f8300856c65
daniel <daniel.socials@gmail.com>
2015-05-29 17:19:05 +0800

    add unconfim tx

--- schema.sql
+++ schema.sql
@@ -82,7 +82,7 @@ ALTER TABLE public.addr_txout OWNER TO bitcoin;
 CREATE TABLE blk (
     id integer NOT NULL,
     hash bytea NOT NULL,
-    depth integer NOT NULL,
+    height integer NOT NULL,
     version bigint NOT NULL,
     prev_hash bytea NOT NULL,
     mrkl_root bytea NOT NULL,
@@ -336,10 +336,10 @@ CREATE INDEX addr_txout_txout_id_index ON addr_txout USING btree (txout_id);
 
 
 --
--- Name: blk_depth_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
+-- Name: blk_height_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
 --
 
-CREATE INDEX blk_depth_index ON blk USING btree (depth);
+CREATE INDEX blk_height_index ON blk USING btree (height);
 
 
 --
@@ -404,6 +404,239 @@ CREATE INDEX tx_hash_index ON tx USING btree (hash);
 
 CREATE INDEX txin_id_p2sh_type_index ON txin USING btree (id, p2sh_type);
 
+
+--mempool create start
+
+
+--
+-- Name: uaddr; Type: TABLE; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE TABLE uaddr (
+    id integer NOT NULL,
+    hash160 text NOT NULL,
+    type integer DEFAULT 0 NOT NULL
+);
+
+ALTER TABLE public.uaddr OWNER TO bitcoin;
+
+--
+-- Name: uaddr_id_seq; Type: SEQUENCE; Schema: public; Owner: bitcoin
+--
+
+CREATE SEQUENCE uaddr_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.uaddr_id_seq OWNER TO bitcoin;
+
+--
+-- Name: uaddr_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: bitcoin
+--
+
+ALTER SEQUENCE uaddr_id_seq OWNED BY uaddr.id;
+
+--
+-- Name: uaddr_txout; Type: TABLE; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE TABLE uaddr_txout (
+    addr_id integer NOT NULL,
+    txout_id integer NOT NULL
+);
+
+
+ALTER TABLE public.uaddr_txout OWNER TO bitcoin;
+
+
+--
+-- Name: utx; Type: TABLE; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE TABLE utx (
+    id integer NOT NULL,
+    hash bytea NOT NULL,
+    version bigint NOT NULL,
+    lock_time bigint NOT NULL,
+    coinbase boolean NOT NULL,
+    tx_size integer NOT NULL,
+    nhash bytea
+);
+
+
+ALTER TABLE public.utx OWNER TO bitcoin;
+
+--
+-- Name: utx_id_seq; Type: SEQUENCE; Schema: public; Owner: bitcoin
+--
+
+CREATE SEQUENCE utx_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.utx_id_seq OWNER TO bitcoin;
+
+--
+-- Name: utx_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: bitcoin
+--
+
+ALTER SEQUENCE utx_id_seq OWNED BY utx.id;
+
+
+--
+-- Name: utxin; Type: TABLE; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE TABLE utxin (
+    id integer NOT NULL,
+    tx_id integer NOT NULL,
+    tx_idx integer NOT NULL,
+    prev_out_index bigint NOT NULL,
+    sequence bigint NOT NULL,
+    script_sig bytea,
+    prev_out bytea,
+    p2sh_type integer
+);
+
+
+ALTER TABLE public.utxin OWNER TO bitcoin;
+
+--
+-- Name: utxin_id_seq; Type: SEQUENCE; Schema: public; Owner: bitcoin
+--
+
+CREATE SEQUENCE utxin_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.utxin_id_seq OWNER TO bitcoin;
+
+--
+-- Name: utxin_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: bitcoin
+--
+
+ALTER SEQUENCE utxin_id_seq OWNED BY utxin.id;
+
+
+--
+-- Name: utxout; Type: TABLE; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE TABLE utxout (
+    id integer NOT NULL,
+    tx_id integer NOT NULL,
+    tx_idx integer NOT NULL,
+    pk_script bytea NOT NULL,
+    value bigint,
+    type integer NOT NULL
+);
+
+
+ALTER TABLE public.utxout OWNER TO bitcoin;
+
+--
+-- Name: utxout_id_seq; Type: SEQUENCE; Schema: public; Owner: bitcoin
+--
+
+CREATE SEQUENCE utxout_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.utxout_id_seq OWNER TO bitcoin;
+
+--
+-- Name: utxout_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: bitcoin
+--
+
+ALTER SEQUENCE utxout_id_seq OWNED BY utxout.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: bitcoin
+--
+
+ALTER TABLE ONLY uaddr ALTER COLUMN id SET DEFAULT nextval('uaddr_id_seq'::regclass);
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: bitcoin
+--
+
+ALTER TABLE ONLY utx ALTER COLUMN id SET DEFAULT nextval('utx_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: bitcoin
+--
+
+ALTER TABLE ONLY utxin ALTER COLUMN id SET DEFAULT nextval('utxin_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: bitcoin
+--
+
+ALTER TABLE ONLY utxout ALTER COLUMN id SET DEFAULT nextval('utxout_id_seq'::regclass);
+
+
+--
+-- Name: uaddr_hash160_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE INDEX uaddr_hash160_index ON uaddr USING btree (hash160);
+
+
+--
+-- Name: uaddr_hash160_type_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE INDEX uaddr_hash160_type_index ON uaddr USING btree (hash160, type);
+
+
+--
+-- Name: uaddr_txout_addr_id_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE INDEX uaddr_txout_addr_id_index ON uaddr_txout USING btree (addr_id);
+
+
+--
+-- Name: uaddr_txout_txout_id_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE INDEX uaddr_txout_txout_id_index ON uaddr_txout USING btree (txout_id);
+
+
+--
+-- Name: utx_hash_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE INDEX utx_hash_index ON utx USING btree (hash);
+
+
+--
+-- Name: utxin_id_p2sh_type_index; Type: INDEX; Schema: public; Owner: bitcoin; Tablespace: 
+--
+
+CREATE INDEX utxin_id_p2sh_type_index ON utxin USING btree (id, p2sh_type);
+ 
+
+--mempool create end
 
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
