c09351467dd8e2abce5e2bd40c698a6d58864acd
daniel <daniel.socials@gmail.com>
2015-09-16 09:58:44 +0800

    update

--- api/demo.py
+++ api/demo.py
@@ -11,85 +11,94 @@ from sqlalchemy import and_
 app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
 
-def buffer_to_json(python_object):
-    if isinstance(python_object, (buffer, )):
-        return binascii.hexlify(python_object)
-    raise TypeError(repr(python_object) + ' is not JSON serializable')
-
 @app.route('/')
 def home():                                                                                                                                                                  
     return render_template('home.html')
 
 @app.route('/tx/<txhash>')
 def tx(txhash):
-    txhash = txhash.decode('hex')
-    res = Tx.query.filter(Tx.hash == txhash).first()
+    res = Tx.query.filter(Tx.hash == txhash.decode('hex')).first()
+    if res== None:
+        return render_template('404.html'), 404
     tx= res.todict()
-    tx_id = tx['id']
 
-    txins = TxIn.query.filter(TxIn.tx_id==tx_id).all()
+    txins = TxIn.query.filter(TxIn.tx_id==tx['id']).all()
     tx['vin'] = [txin.todict() for txin in txins ]
-    for txin in tx['vin']:
-        txin['address']=VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txin_tx_id==txin['id']).all()
-    txouts = TxOut.query.filter(TxOut.tx_id==tx_id).all()
+    txouts = TxOut.query.filter(TxOut.tx_id==tx['id']).all()
     tx['vout'] = [txout.todict() for txout in txouts]
-    for txout in tx['vin']:
-        txout['address'] = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txout_tx_id==txout['id']).all()
+
+    tx['in_addresses'] = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txin_tx_id==tx['id']).all()
+    tx['out_addresses'] = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txout_tx_id==tx['id']).all()
+ 
     return render_template("tx.html",tx=tx)
 
 @app.route('/height/<height>')
 def blkheight(height=0):
     res = Block.query.filter(Block.height == height).first()
+    if res== None:
+        return render_template('404.html'), 404
+
     blk = res.todict()
-    res = Block.query.with_entities(Block.hash).filter(Block.height == (int(height)-1)).first()
-    blk['previousblockhash']=binascii.hexlify(res[0])
-    res = Block.query.with_entities(Block.hash).filter(Block.height == (int(height)+1)).first()
-    blk['nextblockhash']=binascii.hexlify(res[0])
+
     res = BlockTx.query.with_entities(BlockTx.tx_id).filter(BlockTx.blk_id == blk['id']).limit(10)
-
-    txs=[]
-    for txid in res:
-       res = Tx.query.filter(Tx.id==txid).first()
-       tx= res.todict()
-       txins = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txin_tx_id==txid).all()
-       tx['in'] = txins
-       txouts = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txout_tx_id==txid).all()
-       tx['out'] = txouts
-       txs.append(tx)
-
-
+    if res!= None:
+        txs=[]
+        for txid in res:
+           res = Tx.query.filter(Tx.id==txid).first()
+           tx= res.todict()
+           txins = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txin_tx_id==txid).all()
+           tx['in'] = txins
+           txouts = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txout_tx_id==txid).all()
+           tx['out'] = txouts
+           txs.append(tx)
     blk['tx']=txs
+
+    res = Block.query.with_entities(Block.hash).filter(Block.height == (int(height)-1)).first()
+    if res!= None:
+        blk['previousblockhash']=binascii.hexlify(res[0])
+    res = Block.query.with_entities(Block.hash).filter(Block.height == (int(height)+1)).first()
+    if res!= None:
+        blk['nextblockhash']=binascii.hexlify(res[0])
+
     return render_template("blk.html",blk=blk)
 
 @app.route('/blk/<blkhash>')
 def blk(blkhash):
     res = Block.query.filter(Block.hash == blkhash.decode('hex')).first()
-    blk = res.todict()
-    res = Block.query.with_entities(Block.hash).filter(Block.height == (int(blk['height'])-1)).first()
-    blk['previousblockhash']=binascii.hexlify(res[0])
-    res = Block.query.with_entities(Block.hash).filter(Block.height == (int(blk['height'])+1)).first()
-    blk['nextblockhash']=binascii.hexlify(res[0])
-    res = BlockTx.query.with_entities(BlockTx.tx_id).filter(BlockTx.blk_id == blk['id']).limit(10)
+    if res== None:
+        return render_template('404.html'), 404
 
-    txs=[]
-    for txid in res:
-       res = Tx.query.filter(Tx.id==txid).first()
-       tx= res.todict()
-       txins = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txin_tx_id==txid).all()
-       tx['in'] = txins
-       txouts = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txout_tx_id==txid).all()
-       tx['out'] = txouts
-       txs.append(tx)
+    blk = res.todict()
+
+    res = BlockTx.query.with_entities(BlockTx.tx_id).filter(BlockTx.blk_id == blk['id']).limit(10)
+    if res!= None:
+        txs=[]
+        for txid in res:
+           res = Tx.query.filter(Tx.id==txid).first()
+           tx= res.todict()
+           txins = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txin_tx_id==txid).all()
+           tx['in'] = txins
+           txouts = VOUT.query.with_entities(VOUT.address, VOUT.value).filter(VOUT.txout_tx_id==txid).all()
+           tx['out'] = txouts
+           txs.append(tx)
 
     blk['tx']=txs
+
+    res = Block.query.with_entities(Block.hash).filter(Block.height == (int(blk['height'])+1)).first()
+    if res!= None:
+        blk['nextblockhash']=binascii.hexlify(res[0])
+
     return render_template("blk.html",blk=blk)
  
 @app.route('/addr/<address>')
 @app.route('/addr/<address>/<page>')
 def address(address, page=0, num=10):
+    page =int(page)
     res = Addr.query.filter(Addr.address == address).first()
+    if res== None:
+        return render_template('404.html'), 404
     addr=res.todict()
-    txidlist = VOUT.query.with_entities(VOUT.txout_tx_id).filter(VOUT.address == address).limit(10)
+    txidlist = VOUT.query.with_entities(VOUT.txout_tx_id).filter(VOUT.address == address).offset(page*10).limit(10)
     #txidlist = VOUT.query.with_entities(VOUT.txout_tx_id).filter(and_(VOUT.address == address, VOUT.txin_tx_id==None )).limit(10)
     #if txidlist == None:
 
@@ -105,7 +114,6 @@ def address(address, page=0, num=10):
  
     addr['txs']=txs
     addr['address']=address
-    print addr
 
     return render_template("addr.html", addr=addr,page=page)
 
