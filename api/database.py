b48d2df3d604fbf389d8d108b43b3b4948ba540d
philsong <songbohr@gmail.com>
2015-09-18 14:28:34 +0800

    fix height.rm used field in mode.

--- api/database.py
+++ api/database.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import relationship, backref
 #logging.basicConfig()
 #logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-engine = create_engine('postgresql://postgres:c1u2u9z@@127.0.0.1:5433/test',
+engine = create_engine('postgresql://postgres:c1u2u9z@@103.249.253.242:5433/test',
                        echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
@@ -76,6 +76,8 @@ class Tx(SQLBase):
     out_count = Column(INTEGER)
     out_value = Column(BIGINT )
     fee       = Column(BIGINT )
+    recv_time = Column(BIGINT )
+    ip = Column(BYTEA )
 
     def todict(self):
         return to_dict(self, self.__class__)
@@ -132,19 +134,12 @@ class Addr(SQLBase):
 class VOUT(SQLBase):
     __tablename__ = 'vout'
     address = Column(TEXT, primary_key=True)
-    hash160 = Column(TEXT)
     addr_id = Column(INTEGER)
     txout_id = Column(INTEGER)
     txin_id = Column(INTEGER)
     txin_tx_id = Column(INTEGER)
     txout_tx_id = Column(INTEGER)
-    txout_txhash = Column(BYTEA)
     value = Column(BIGINT)
-    tx_idx = Column(INTEGER)
-    height = Column(INTEGER)
-    time  = Column(BIGINT)
-    pk_script  = Column(BYTEA)
-    rev_time  = Column(BIGINT)
 
     @property
     def todict(self):
