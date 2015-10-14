e819aba11b19f09828a1f0e7712325c849ccc109
daniel <daniel.socials@gmail.com>
2015-10-14 10:29:43 +0800

    merge

--- api/config.py
+++ api/config.py
@@ -10,7 +10,7 @@ DEBUG = False
 HOST='127.0.0.1'
 PORT=5000
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://user:pass@@127.0.0.1:5432/dbname'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:c1u2u9z@@127.0.0.1:5433/test'
 RPC_URL = "http://bitcoinrpc:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX@127.0.0.1:8332"
 
 
--- api/update2.sql
+++ api/update2.sql
@@ -1,39 +1,39 @@
 DROP FUNCTION delete_height(blkheight integer);
 CREATE FUNCTION delete_height(blkheight integer) RETURNS void
     LANGUAGE plpgsql
-    AS $$                                                                                                                     
-    declare blkhash bytea;                                                                                                
+    AS $$
+    declare blkhash bytea;
     BEGIN
-    blkhash = (select hash from blk where height=blkheight);                                                                                                                                                                                                                                                                                       
-    perform delete_blk(blkhash);                                                                                                  
-    END                                                                                                                       
+    blkhash = (select hash from blk where height=blkheight);
+    perform delete_blk(blkhash);
+    END
 $$;
 
-CREATE FUNCTION delete_all_utx() RETURNS void                                                                                 
-    LANGUAGE plpgsql                                                                                                          
-    AS $$                                                                                                                     
-    DECLARE txid integer;                                                                                                     
-BEGIN                                                                                                                         
-     FOR txid IN select id from utx LOOP                                                                                      
-         perform delete_tx(txid);                                                                                             
-     END LOOP;                                                                                                                
-END;                                                                                                                          
-$$; 
+CREATE FUNCTION delete_all_utx() RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+    DECLARE txid integer;
+BEGIN
+     FOR txid IN select id from utx LOOP
+         perform delete_tx(txid);
+     END LOOP;
+END;
+$$;
 
 DROP FUNCTION delete_height_from(blkheight integer);
 CREATE FUNCTION delete_height_from(blkheight integer) RETURNS void
     LANGUAGE plpgsql
-    AS $$                                                                                                                     
+    AS $$
     DECLARE blkhash bytea;
-    DECLARE max_height integer;     
-    DECLARE curheight integer;                                                                                              
+    DECLARE max_height integer;
+    DECLARE curheight integer;
     BEGIN
-        max_height = (select max(height) from blk);   
-        LOOP                                                                          
+        max_height = (select max(height) from blk);
+        LOOP
             IF blkheight <= max_height THEN
                 curheight=max_height;
-                blkhash = (select hash from blk where height=curheight);                                                                                                                                                                                                                                                                                       
-                perform delete_blk(blkhash); 
+                blkhash = (select hash from blk where height=curheight);
+                perform delete_blk(blkhash);
                 max_height = max_height - 1;
             ELSE
                 return;
@@ -66,4 +66,3 @@ AS $$
         END LOOP;
     END;
 $$
-LANGUAGE plpgsql;
