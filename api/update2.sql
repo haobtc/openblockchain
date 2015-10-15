f66e9bd73845a894290baf406d8b4c9b98f893cc
PhilSong <songbohr@gmail.com>
2015-10-15 18:05:16 +0800

    add check script

--- api/update2.sql
+++ api/update2.sql
@@ -67,3 +67,85 @@ AS $$
     END;
 $$
 LANGUAGE plpgsql;
+
+drop FUNCTION check_tx();
+CREATE FUNCTION check_tx() RETURNS BOOL
+AS $$
+    DECLARE tx_count bigint;        
+    DECLARE utx_count bigint;   
+    DECLARE blk_tx_count bigint;   
+    DECLARE blk_tx_id_count bigint;   
+
+    BEGIN
+        blk_tx_count = (select sum(blk.tx_count) from blk);
+        blk_tx_id_count = (select count(distinct(tx_id)) from blk_tx);
+        IF blk_tx_count != blk_tx_id_count+2 THEN
+            RAISE LOG 'blk_tx_count(%) != blk_tx_id_count(%) ', blk_tx_count, blk_tx_id_count;
+            return FALSE;
+        END IF;
+
+        tx_count = (select count(1) from tx);
+        utx_count = (select count(1) from utx);
+        IF tx_count + 2 != utx_count + blk_tx_count
+        THEN
+            RAISE LOG 'tx_count(%) + 2 != utx_count(%) + blk_tx_count(%)', tx_count, utx_count, blk_tx_count;
+            return FALSE;
+        END IF;
+
+        RAISE LOG 'pass tx check % % %', blk_tx_count, tx_count, utx_count;
+        return TRUE;
+    END;
+$$
+LANGUAGE plpgsql;
+
+
+
+drop FUNCTION check_blk();
+CREATE FUNCTION check_blk() RETURNS BOOL
+AS $$
+    DECLARE blk_height bigint;        
+    DECLARE blk_count bigint;   
+    DECLARE blk_id_count bigint;   
+
+    BEGIN
+        blk_height = (select max(height) from blk);
+        blk_count = (select count(1) from blk);
+        blk_id_count = (select count(distinct(blk_id)) from blk_tx);
+        IF blk_count != blk_id_count THEN
+            RAISE LOG 'blk_count != blk_id_count ';
+            return FALSE;
+        END IF;
+
+        IF blk_count != blk_height + 1
+        THEN
+            RAISE LOG 'blk_count != blk_height + 1';
+            return FALSE;
+        END IF;
+
+        RAISE LOG 'pass blk check';
+        return TRUE;
+    END;
+$$
+LANGUAGE plpgsql;
+
+drop FUNCTION check_db();
+CREATE FUNCTION check_db() RETURNS BOOL
+AS $$
+    DECLARE blk_ok BOOL; 
+    DECLARE tx_ok BOOL; 
+    BEGIN
+        blk_ok = (select check_blk());
+        IF NOT blk_ok THEN
+            return FALSE;
+        END IF;
+
+        tx_ok = (select check_tx());
+        IF NOT tx_ok THEN
+            return FALSE;
+        END IF;
+
+        return TRUE;
+    END;
+$$
+LANGUAGE plpgsql;
+
