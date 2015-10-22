f319af16e36b0a44b5406c847f38f2af47c67c18
PhilSong <songbohr@gmail.com>
2015-10-22 11:51:25 +0800

    add email notify and fix tx script.

--- api/config.py
+++ api/config.py
@@ -14,3 +14,9 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:c1u2u9z@@127.0.0.1:5433/test'
 RPC_URL = "http://bitcoinrpc:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX@127.0.0.1:8332"
 
 
+EMAIL_HOST = 'p.haobtc.com'
+EMAIL_HOST_USER = 'notify@haobtc.com'
+EMAIL_HOST_PASSWORD = 'notify.asd'
+EMAIL_USE_TLS = True
+
+EMAIL_RECEIVER = ['songbohr@163.com']
