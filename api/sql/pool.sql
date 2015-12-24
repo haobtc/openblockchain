4f8b010e0fa375ed6c8cfafe09be7989ef1ff8f9
daniel <daniel.socials@gmail.com>
2015-12-24 09:40:36 +0800

    add pool info sql

---
+++ api/sql/pool.sql
@@ -0,0 +1,70 @@
+CREATE TABLE pool (id serial, name text NOT NULL, link text);
+COPY	pool	(id,	name,	link)	FROM	stdin;
+1	MegaBigPower	\N
+2	BTCChina Pool	\N
+3	Huobi	\N
+4	Polmine	\N
+11	8baochi	\N
+12	F2Pool	\N
+13	Telco 214	\N
+14	digitalX Mintsy	\N
+15	Bitsolo	\N
+16	Cointerra	\N
+17	EclipseMC	\N
+18	21 Inc.	\N
+21	BTC Nuggets	\N
+22	myBTCcoin Pool	\N
+23	BTC Pool Party	\N
+25	GHash.IO	\N
+26	CloudHashing	\N
+27	BW.COM	\N
+28	digitalBTC	\N
+29	BitMinter	\N
+31	BitFury	\N
+35	BitClub Network	\N
+36	Bitalo	\N
+37	TBDice	\N
+38	Slush	\N
+47	AntPool	\N
+49	mmpool	\N
+50	OzCoin	\N
+54	Bitcoin Affiliate Network	\N
+57	TripleMining	\N
+58	Give Me Coins	\N
+59	BTC Guild	\N
+60	Solo CKPool	\N
+64	Mt Red	\N
+65	NMCbit	\N
+67	175btc	\N
+69	ASICMiner	\N
+70	simplecoin.us	\N
+71	50BTC	\N
+72	Bitparking	\N
+74	ST Mining Corp	\N
+78	bcpool.io	\N
+79	Kano CKPool	\N
+81	Eligius	\N
+84	MultiCoin.co	\N
+86	BTCServ	\N
+87	NiceHash Solo	\N
+88	MaxBTC	\N
+91	CoinLab	\N
+93	Yourbtc.net	\N
+94	KnCMiner	\N
+95	HHTT	\N
+96	stratumPool	\N
+97	nodeStratum	\N
+98	p2pool	\N
+99	ckpool	\N
+100	multipool	"https://www.multipool.us"
+101	mybitcoin	http://mybtccoin.com
+102	btcdig	https://btcdig.com/stats/blocks
+103	wizkid057	\N
+104	2av0id51pct	\N
+105	hashpool	http://hashpool.com
+106	transactioncoinmining	http://sha256.transactioncoinmining.com
+107	a-xbt	http://www.a-xbt.com
+108	nexious	https://nexious.com
+109	bravo	http://www.bravo-mining.com/
+110	trickys	http://pool.wemine.uk/
+\.
