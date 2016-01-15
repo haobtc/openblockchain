5751b7ae2a49f24c14876acb2cbfed64183d962c
daniel <daniel.socials@gmail.com>
2016-01-15 10:34:49 +0800

    add blk bip support

---
+++ api/sql/bip.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bip (id serial, name text NOT NULL, link text);
+COPY	bip (id,	name,	link)	FROM	stdin;
+1	default	\N
+2	BIP_8M	\N
+3	BIP_100	\N
+4	BIP_101_8M	\N
+5	BIP_101_2M	\N
+5	BIP_248	\N
+\.
